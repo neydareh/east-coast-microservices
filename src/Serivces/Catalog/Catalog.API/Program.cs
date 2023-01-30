5d5d034745d7108d16c45773715ccbeb78588b22
Olakunle Neye <olakunle.neye@thoughtworks.com>
2023-01-29 19:54:19 -0600

    adds dockerfile, docker compose and docker launch settings

--- src/Serivces/Catalog/Catalog.API/Program.cs
+++ src/Serivces/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,6 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,15 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseDeveloperExceptionPage();
+  app.UseSwagger();
+  app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
 }
+
+app.UseRouting();
 
 app.UseAuthorization();
 

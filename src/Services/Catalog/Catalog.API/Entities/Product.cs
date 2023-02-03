c4ac509c65ae47b306e8f037bdc0c4e487629ba4
Olakunle Neye <olakunle.neye@thoughtworks.com>
2023-02-03 10:34:12 -0600

    adds bson annotation to the products entity

--- src/Services/Catalog/Catalog.API/Entities/Product.cs
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -13,12 +13,17 @@ namespace Catalog.API.Entities
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
-    [BsonElement("Name")]
+    [BsonElement("name")]
     public string Name { get; set; }
+    [BsonElement("category")]
     public string Category { get; set; }
+    [BsonElement("summary")]
     public string Summary { get; set; }
+    [BsonElement("description")]
     public string Description { get; set; }
+    [BsonElement("image_file")]
     public string ImageFile { get; set; }
+    [BsonElement("price")]
     public decimal Price { get; set; }
   }
 }

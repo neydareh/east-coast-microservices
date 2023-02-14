3226660310aee531e90cf1fba3e5bc30ba45f24e
Olakunle E Neye <emmanuelneye@gmail.com>
2023-02-13 21:19:39 -0600

    adds catalog service to the project

---
+++ src/Services/Catalog/Catalog.API/Services/CatalogService.cs
@@ -0,0 +1,51 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+
+namespace Catalog.API.Services {
+  public class CatalogService : ICatalogService {
+    private IProductRepository _repository;
+
+    public CatalogService(IProductRepository repository)
+    {
+      _repository = repository ?? throw new ArgumentNullException(nameof(repository));  
+    }
+    public async Task CreateProduct(Product product)
+    {
+      await _repository.CreateProduct(product);
+    }
+
+    public async Task<bool> DeleteProduct(string id)
+    {
+      return await _repository.DeleteProduct(id);
+    }
+
+    public async Task<Product> GetProduct(string id)
+    {
+      return await _repository.GetProduct(id);
+    }
+
+    public async Task<List<Product>> GetProductByCategory(string categoryName)
+    {
+      var enumerableList = await _repository.GetProductByCategory(categoryName);
+      List<Product> products = enumerableList.ToList();
+      return products;
+    }
+
+    public async Task<List<Product>> GetProductByName(string name)
+    {
+      var enumerableList = await _repository.GetProductByName(name);
+      return enumerableList.ToList();
+    }
+
+    public async Task<List<Product>> GetProducts()
+    {
+      var enumerableList = await _repository.GetProducts();
+      return enumerableList.ToList();
+    }
+
+    public async Task<bool> UpdateProduct(Product product)
+    {
+      return await _repository.UpdateProduct(product);
+    }
+  }
+}
---
+++ src/Services/Catalog/Catalog.API/Services/ICatalogService.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Services {
+
+  public interface ICatalogService {
+    Task<List<Product>> GetProducts();
+    Task<Product> GetProduct(string id);
+    Task<List<Product>> GetProductByName(string name);
+    Task<List<Product>> GetProductByCategory(string categoryName);
+    Task CreateProduct(Product product);
+    Task<bool> UpdateProduct(Product product);
+    Task<bool> DeleteProduct(string id);
+  }
+}

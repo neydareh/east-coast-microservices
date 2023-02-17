2922c18173bdb4039b483f1a4b8ccb9a7253e1a0
Olakunle E Neye <emmanuelneye@gmail.com>
2023-02-17 12:11:38 -0600

    refactors discount GRPC program.cs

--- src/Services/Discount/Discount.GRPC/Program.cs
+++ src/Services/Discount/Discount.GRPC/Program.cs
@@ -5,10 +5,6 @@ using Discount.GRPC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
 builder.Services.AddGrpc();
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddAutoMapper(typeof(Program));
@@ -19,7 +15,6 @@ var app = builder.Build();
 // Seeds the DB after building the app
 app.MigrateDB<Program>();
 
-// Configure the HTTP request pipeline.
 app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 

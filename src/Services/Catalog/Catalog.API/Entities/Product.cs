5a21839a87a931ce5358763cefcf1e3a02cf0847
Olakunle Neye <olakunle.neye@thoughtworks.com>
2023-02-22 16:16:19 -0600

    fixes mongo bson element qualifier

--- src/Services/Catalog/Catalog.API/Entities/Product.cs
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -7,18 +7,16 @@ namespace Catalog.API.Entities {
   {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
-    [BsonElement("name")]
-    public string Name { get; set; }
-    [BsonElement("category")]
-    public string Category { get; set; }
-    [BsonElement("summary")]
-    public string Summary { get; set; }
-    [BsonElement("description")]
-    public string Description { get; set; }
-    [BsonElement("image_file")]
-    public string ImageFile { get; set; }
-    [BsonElement("price")]
+    public string? Id { get; set; }
+    [BsonElement("Name")]
+    public string? Name { get; set; }
+    [BsonElement("Category")]
+    public string? Category { get; set; }
+    [BsonElement("Summary")]
+    public string? Summary { get; set; }
+    [BsonElement("Description")]
+    public string? Description { get; set; }
+    public string? ImageFile { get; set; }
     public decimal Price { get; set; }
   }
 }

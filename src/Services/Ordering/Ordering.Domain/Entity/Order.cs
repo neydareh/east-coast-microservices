07eb406960459d80bf1d661662290fea8042e599
Olakunle E Neye <emmanuelneye@gmail.com>
2023-02-28 19:01:29 -0600

    adds domain entities for Ordering.Domain

---
+++ src/Services/Ordering/Ordering.Domain/Entity/Order.cs
@@ -0,0 +1,11 @@
+﻿using Ordering.Domain.Common;
+
+namespace Ordering.Domain.Entity {
+  public class Order : EntityBase {
+    public string? UserName { get; set; }
+    public decimal TotalPrice { get; set; }
+
+    public Billing? Billing { get; set; }
+    public Payment? Payment { get; set; }
+  }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Entity/Payment.cs
@@ -0,0 +1,6 @@
+﻿namespace Ordering.Domain.Entity {
+  public class Payment {
+    public string CardName { get; set; }
+    public string CardNumber { get; set; }
+  }
+}

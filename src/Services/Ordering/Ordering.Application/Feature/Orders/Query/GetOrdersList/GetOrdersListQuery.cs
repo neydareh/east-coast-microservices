0cf9dc4417f36e66c99dcc320c1376613ebb8bef
Olakunle E Neye <emmanuelneye@gmail.com>
2023-02-28 23:50:07 -0600

    adds GetOrdersList Query to Orders feature directory

---
+++ src/Services/Ordering/Ordering.Application/Feature/Orders/Query/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Feature.Orders.Query.GetOrdersList {
+  public class GetOrdersListQuery : IRequest<List<OrdersVm>> {
+    public string? UserName { get; set; }
+
+    public GetOrdersListQuery(string? userName)
+    {
+      UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+    }
+  }
+}
---
+++ src/Services/Ordering/Ordering.Application/Feature/Orders/Query/GetOrdersList/OrdersVm.cs
@@ -0,0 +1,21 @@
+﻿using Ordering.Domain.Entity;
+namespace Ordering.Application.Feature.Orders.Query.GetOrdersList {
+  public class OrdersVm {
+    public string? UserName { get; set; }
+    public decimal TotalPrice { get; set; }
+
+    public Billing? Billing { get; set; }
+    public Payment? Payment { get; set; }
+
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? EmailAddress { get; set; }
+    public string? Address { get; set; }
+    public string? Country { get; set; }
+    public string? State { get; set; }
+    public string? ZipCode { get; set; }
+
+    public string? CardName { get; set; }
+    public string? CardNumber { get; set; }
+  }
+}

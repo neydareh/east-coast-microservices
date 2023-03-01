7770f3b080aa36ef2d1d4237901ff4f2492f4315
Olakunle E Neye <emmanuelneye@gmail.com>
2023-02-28 23:33:17 -0600

    fixes nullable class properties

--- src/Services/Ordering/Ordering.Domain/Entity/Billing.cs
+++ src/Services/Ordering/Ordering.Domain/Entity/Billing.cs
@@ -1,11 +1,11 @@
 ﻿namespace Ordering.Domain.Entity {
   public class Billing {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string EmailAddress { get; set; }
-    public string Address { get; set; }
-    public string Country { get; set; }
-    public string State { get; set; }
-    public string ZipCode { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? EmailAddress { get; set; }
+    public string? Address { get; set; }
+    public string? Country { get; set; }
+    public string? State { get; set; }
+    public string? ZipCode { get; set; }
   }
 }
--- src/Services/Ordering/Ordering.Domain/Entity/Payment.cs
+++ src/Services/Ordering/Ordering.Domain/Entity/Payment.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ordering.Domain.Entity {
   public class Payment {
-    public string CardName { get; set; }
-    public string CardNumber { get; set; }
+    public string? CardName { get; set; }
+    public string? CardNumber { get; set; }
   }
 }

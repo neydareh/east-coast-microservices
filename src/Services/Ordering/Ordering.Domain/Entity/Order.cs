c03886cf936e3150c3b324ebf511e259c2739b70
Olakunle E Neye <emmanuelneye@gmail.com>
2023-02-28 23:49:03 -0600

    revert Ordering.Domain Order Entity to spread all properties

--- src/Services/Ordering/Ordering.Domain/Entity/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Entity/Order.cs
@@ -7,5 +7,16 @@ namespace Ordering.Domain.Entity {
 
     public Billing? Billing { get; set; }
     public Payment? Payment { get; set; }
+
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? EmailAddress { get; set; }
+    public string? Address { get; set; }
+    public string? Country { get; set; }
+    public string? State { get; set; }
+    public string? ZipCode { get; set; }
+
+    public string? CardName { get; set; }
+    public string? CardNumber { get; set; }
   }
 }

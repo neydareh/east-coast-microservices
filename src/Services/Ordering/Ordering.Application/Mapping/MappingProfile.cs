26e025ebd6a06ff049ff222a1830c7b54b31f286
Olakunle E Neye <emmanuelneye@gmail.com>
2023-02-28 23:50:29 -0600

    adds automapper for mapping Order to OrdersVm

--- src/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
@@ -1,6 +1,12 @@
 ﻿using AutoMapper;
+using Ordering.Application.Feature.Orders.Query.GetOrdersList;
+using Ordering.Domain.Entity;
 
 namespace Ordering.Application.Mapping {
   public class MappingProfile : Profile {
+    public MappingProfile()
+    {
+       CreateMap<Order, OrdersVm>().ReverseMap();
+    }
   }
 }

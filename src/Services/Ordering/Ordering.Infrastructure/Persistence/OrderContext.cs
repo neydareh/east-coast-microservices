fc7aef29aa488eff7095626a7a843579ffe24f3a
Olakunle Neye <olakunle.neye@thoughtworks.com>
2023-03-11 13:49:08 -0600

    adds entity framework core for sql server and create order context with interceptions

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entity;
+
+namespace Ordering.Infrastructure.Persistence;
+
+public class OrderContext : DbContext {
+  public OrderContext(DbContextOptions<OrderContext> options) : base(options) {
+  }
+
+  public DbSet<Order> Orders { get; set; }
+
+  public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken()) {
+    foreach (var entry in ChangeTracker.Entries<EntityBase>()) {
+      switch (entry.State) {
+        case EntityState.Added:
+          entry.Entity.CreatedDate = DateTime.UtcNow;
+          entry.Entity.CreatedBy = "swn";
+          break;
+        case EntityState.Modified:
+          entry.Entity.LastModifiedDate = DateTime.UtcNow;
+          entry.Entity.LastModifiedBy = "swn";
+          break;
+      }
+    }
+
+    return base.SaveChangesAsync(cancellationToken);
+  }
+}

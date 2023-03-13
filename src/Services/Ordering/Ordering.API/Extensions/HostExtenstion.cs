773921882edc15b6d8bada19922c0a9fbfe5340d
Olakunle Neye <olakunle.neye@thoughtworks.com>
2023-03-13 13:21:29 -0500

    update EF Core

--- src/Services/Ordering/Ordering.API/Extensions/HostExtenstion.cs
+++ src/Services/Ordering/Ordering.API/Extensions/HostExtenstion.cs
@@ -1,12 +1,11 @@
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-using Npgsql;
 
 namespace Ordering.API.Extensions;
 
 public static class HostExtenstion {
   public static IHost MigrateDatabase<TContext>(this IHost host, Action<TContext, IServiceProvider> seeder,
     int? retry = 0) where TContext : DbContext {
-    if (retry == null) return host;
     var retryForAvailability = retry.Value;
 
     using var scope = host.Services.CreateScope();
@@ -19,13 +18,13 @@ public static class HostExtenstion {
       InvokeSeeder(seeder, context, services);
       logger.LogInformation($"Migrated database associated with context {typeof(TContext).Name}");
     }
-    catch (NpgsqlException e) {
+    catch (SqlException e) {
       logger.LogError(e, $"An error occurred while migrating the database used on context {typeof(TContext).Name}");
 
       if (retryForAvailability < 5) {
         retryForAvailability++;
         Thread.Sleep(2000);
-        MigrateDatabase(host, seeder, retryForAvailability);
+        MigrateDatabase<TContext>(host, seeder, retryForAvailability);
       }
     }
 

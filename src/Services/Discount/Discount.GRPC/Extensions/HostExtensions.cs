2c8ba447213b8d8448712fe94db65a583cc6752c
Olakunle Neye <olakunle.neye@thoughtworks.com>
2023-04-05 09:59:26 -0500

    changes the seeded value to conform to the lowercase standard

--- src/Services/Discount/Discount.GRPC/Extensions/HostExtensions.cs
+++ src/Services/Discount/Discount.GRPC/Extensions/HostExtensions.cs
@@ -35,10 +35,10 @@ namespace Discount.GRPC.Extensions {
           command.CommandText = @"CREATE TABLE Coupon (Id SERIAL PRIMARY KEY NOT NULL, ProductName VARCHAR(24) NOT NULL, Description TEXT, Amount INT)";
           command.ExecuteNonQuery();
 
-          command.CommandText = "INSERT INTO Coupon (ProductName, Description, Amount) VALUES ('IPhone X', 'IPhone Discount', 150)";
+          command.CommandText = "INSERT INTO Coupon (ProductName, Description, Amount) VALUES ('iphone x', 'IPhone Discount', 150)";
           command.ExecuteNonQuery();
           
-          command.CommandText = "INSERT INTO Coupon (ProductName, Description, Amount) VALUES ('Samsung 10', 'Samsung Discount', 100)";
+          command.CommandText = "INSERT INTO Coupon (ProductName, Description, Amount) VALUES ('samsung 10', 'Samsung Discount', 100)";
           command.ExecuteNonQuery();
 
           logger.LogInformation("Migrated completed!");

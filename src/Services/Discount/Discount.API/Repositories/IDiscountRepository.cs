60c04984b463625e176b46b27f76cfbb74d166e9
Olakunle Neye <olakunle.neye@thoughtworks.com>
2023-04-05 10:14:32 -0500

    adds GetDiscount to the IDiscountRepository.cs interface

--- src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -3,6 +3,7 @@
 namespace Discount.API.Repositories {
   public interface IDiscountRepository {
     Task<Coupon> GetDiscount(string productName);
+    Task<IEnumerable<Coupon>> GetDiscounts();
     Task<bool> CreateDiscount (Coupon coupon);
     Task<bool> UpdateDiscount (Coupon coupon);
     Task<bool> DeleteDiscount (string productName);

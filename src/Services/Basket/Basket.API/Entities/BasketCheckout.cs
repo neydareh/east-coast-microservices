ebb08676bd5cc06d44a240b0af1c16147460abf4
Olakunle Neye <olakunle.neye@thoughtworks.com>
2023-04-07 18:55:56 -0500

    removes TotalPrice attribute from the BasketCheckout.cs entity

--- src/Services/Basket/Basket.API/Entities/BasketCheckout.cs
+++ src/Services/Basket/Basket.API/Entities/BasketCheckout.cs
@@ -3,7 +3,6 @@ namespace Basket.API.Entities;
 public class BasketCheckout
 {
   public string? UserName { get; set; }
-  public decimal TotalPrice { get; set; }
 
   public string? FirstName { get; set; }
   public string? LastName { get; set; }
